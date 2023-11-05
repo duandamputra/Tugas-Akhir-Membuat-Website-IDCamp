b1691f0f820eab0d1e0bedadbe90ecc663db7bbe
duandamputra <duandamahaputra1@gmail.com>
2023-11-05 11:48:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tugas-Akhir-Membuat-Website-IDCamp
+ 
---
+++ index.js
@@ -0,0 +1,4 @@
+const btn = document.getElementsByClassName("btn")[0];
+  btn.addEventListener("click", function () {
+  alert("Welcome in Duanda's Web");
+});
